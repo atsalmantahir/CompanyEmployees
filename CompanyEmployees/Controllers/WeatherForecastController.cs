1fcd63d2440fa8643d954c60cb6565d899ee75e5
atstahir <atstahir@outlook.com>
2024-05-07 02:25:09 +0500

    using NLoger

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
@@ -9,11 +10,11 @@ namespace CompanyEmployees.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILoggerManager _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
@@ -21,6 +22,12 @@ namespace CompanyEmployees.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is debug message from our values controller.");
+            _logger.LogWarning("Here is warn message from our values controller.");
+            _logger.LogError("Here is an error message from our values controller.");
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
