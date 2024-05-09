f108b60f319f839804132a4df91ef75c5ca662c9
atstahir <atstahir@outlook.com>
2024-05-10 02:06:57 +0500

    added service layer

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -8,10 +8,12 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
 
 // Add services to the container.
 
+builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Service/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+internal sealed class EmployeeService : IEmployeeService
+{
+
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+    public EmployeeService(IRepositoryManager repositoryManager, ILoggerManager logger)
+    {
+        _repository = repositoryManager;
+        _logger = logger;
+    }
+}
