93a734aa2990228d264c021bd44a2202e74cdf65
atstahir <atstahir@outlook.com>
2024-09-04 01:14:12 +0500

    added repo code

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -11,16 +11,29 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     }
 
 
-    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
-        FindAll(trackChanges)
-        .OrderBy(c => c.Name)
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+    {
+        return FindAll(trackChanges)
+         .OrderBy(c => c.Name)
+         .ToList();
+    }
+    public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges)
+    {
+        return FindByCondition(x => ids.Contains(x.Id), trackChanges)
         .ToList();
+    }
 
-    public Company GetCompany(Guid companyId, bool trackChanges) =>
-         FindByCondition(c => c.Id.Equals(companyId), trackChanges)
-         .SingleOrDefault();
 
-    public void CreateCompany(Company company) => Create(company);
+    public Company GetCompany(Guid companyId, bool trackChanges)
+    {
+        return FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+        .SingleOrDefault();
+    }
+
+    public void CreateCompany(Company company) 
+    {
+        Create(company); 
+    }
 
 }
 
